0cb72da92ff4e19b423adc60ee1d45760eac0b8e
Burak <burak.ertuerk@stud.uni-goettingen.de>
2022-11-07 13:08:20 +0100

    updated tailwind config to fix infinite recompiles
    fixed an issue which caused the app to compile every second, caused by leftover code while trying to initially configure the  tailwind config

--- frontend/scraperfrontend/tailwind.config.js
+++ frontend/scraperfrontend/tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/components/Listings/Listings.js",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-    './*/*.html', 
+
 
   ],
   theme: {
