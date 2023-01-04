b71b29fbb9e64c733fd61aa419a17016b9d4ddbb
Burak <burak.ertuerk@stud.uni-goettingen.de>
2023-01-04 22:29:42 +0100

    fixed signupform
    fixed the error which caused the server to crash whenever there was no password entered

--- frontend/src/components/SignUpForm/SignUpForm.js
+++ frontend/src/components/SignUpForm/SignUpForm.js
@@ -5,73 +5,88 @@ import { useState } from 'react';
 
 
 function SignUpForm() {
-    const [formData, setFormData] = useState({
-      email: '',
-      password: '',
-      confirmPassword: '',
-    });
-  
-    const handleChange = (event) => {
-      const { name, value } = event.target;
-      setFormData((prevState) => ({
-        ...prevState,
-        [name]: value,
-      }));
-    };
-  
-    async function handleSubmit(event) {
-        event.preventDefault();
-        try {
-          const response = await fetch('http://localhost:8002/createUser', {
-            method: 'POST',
-            body: JSON.stringify(formData),
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          });
-          const data = await response.json();
-          console.log(data);
-        if (response) {
-          window.location.assign('http://localhost:3000/App')
-          
-        } else {
-          throw new Error('Condition not met')
-          
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    name: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!formData.password) {
+      throw new Error('Password is required')
+    }
+    
+    try {
+      const response = await fetch('http://localhost:8002/createUser', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json'
         }
-        } catch (error) {
-          console.error(error);
-        }
+      });
+      const data = await response.json();
+      console.log(data);
+      if (response) {
+        window.location.assign('http://localhost:3000/App')
+
+      } else {
+        throw new Error('Condition not met')
+
       }
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <label >Email</label>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        <br />
-        <label >Password</label>
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        <br />
-        <label >Confirm Password</label>
-        <input
-          type="password"
-          name="confirmPassword"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-        />
-        <br />
-        <button type="submit">Sign Up</button>
-      </form>
-    );
+    } catch (error) {
+      console.error(error);
+    }
   }
+
+  return (
+    <div class=" container flex items-center bg-teal-700 h-screen">
+      <div class="container flex items-center justify-center">
+        <div >
+          <div class="justify-center flex "><h1 class="text-white">Join EbayNinja for free!</h1></div>
+          
+        <form class="justify-center flex-row items-center"onSubmit={handleSubmit}>
+        <br />
+          <input
+            type="text"
+            name="name"
+            placeholder='Username'
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="email"
+            name="email"
+            placeholder='Email adress'
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="password"
+            name="password"
+            placeholder='Password'
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <br/>
+          <div class="flex items-center justify-center ">  <button class="justify-center items-center text-white text-lg hover:text-teal-500" type="submit">Sign Up</button> </div>
+        
+        </form>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default SignUpForm
--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,105 +1,105 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 export default function Header() {
-    return (
-<div class="sticky top-0 z-50 border-bottom-black">
- <header class="bg-gray-100 border-b-4 border-teal-600">
-  <div class="flex items-center h-16 max-w-screen-xl gap-8 px-4 mx-auto sm:px-6 lg:px-8">
-    
-    <a class=" antialiased text-teal-600 text-lg font-bold transition hover:text-gray-500/75" href="/">
-              Ebay Ninja
-    </a>
-    
+  return (
+    <div class="sticky top-0 z-50 border-bottom-black">
+      <header class="bg-gray-100 border-b-4 border-teal-600">
+        <div class="flex items-center h-16 max-w-screen-xl gap-8 px-4 mx-auto sm:px-6 lg:px-8">
 
-    <div class="flex items-center justify-end flex-1 md:justify-between">
-      <nav class="hidden md:block" aria-labelledby="header-navigation">
-        <h2 class="sr-only" id="header-navigation">Header navigation</h2>
-
-        <ul class="flex items-center gap-6 ">
-
-          <li>  
-            <a class="text-gray-900 font-normal text-base transition hover:text-gray-500/75" href="/">
-              New Listing
-            </a>
-          </li>
-
-          <li>
-            <Link class="text-gray-900 font-normal text-base transition hover:text-gray-500/75" to="/search">
-              New Search
-            </Link>
-          </li>
-
-          <li>
-            <a class="text-gray-900 font-normal text-base transition hover:text-gray-500/75" href="/">
-              Search Finder
-            </a>
-          </li>
-
-          <li>
-            <a class="text-gray-900 font-normal text-base transition hover:text-gray-500/75" href="/">
-              Settings
-            </a>
-          </li>
-          <li>
-            <a href="/">
-              <div class="relative mr-3">
-                  <input
-                   class="block px-5 py-2.5  pr-16 text-sm border-1.5 border-teal-600 rounded-lg"
-                   id="search-url"
-                   type="url"
-                   placeholder="Insert search URL here"
-                   />
-
-                  <button class="absolute p-2 text-white -translate-y-1/2 bg-teal-600 rounded-full top-1/2 right-4" type="button">
-                    <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="white ">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                    </svg>
-                  </button>
-              </div> 
-            </a>
-          </li>
-
-        </ul>
-      </nav>
-
-      <div class="flex items-center gap-4">
-        <div class="sm:gap-4 sm:flex">
-          <a
-            class="block px-5 py-2.5 text-sm font-medium text-white bg-teal-600 hover:bg-teal-700 transition rounded-md"
-            href="/"
-          >
-            Login
+          <a class=" antialiased text-teal-600 text-lg font-bold transition hover:text-gray-500/75" href="/">
+            Ebay Ninja
           </a>
 
-          <a
-            class="hidden sm:block px-5 py-2.5 text-sm font-medium text-teal-600 bg-gray-100 rounded-md hover:text-teal-600/75 transition"
-            href="/signuppage">
-            Register
-          </a>
+
+          <div class="flex items-center justify-end flex-1 md:justify-between">
+            <nav class="hidden md:block" aria-labelledby="header-navigation">
+              <h2 class="sr-only" id="header-navigation">Header navigation</h2>
+
+              <ul class="flex items-center gap-6 ">
+
+                <li>
+                  <a class="text-gray-900 font-normal text-base transition hover:text-gray-500/75" href="/">
+                    New Listing
+                  </a>
+                </li>
+
+                <li>
+                  <Link class="text-gray-900 font-normal text-base transition hover:text-gray-500/75" to="/search">
+                    New Search
+                  </Link>
+                </li>
+
+                <li>
+                  <a class="text-gray-900 font-normal text-base transition hover:text-gray-500/75" href="/">
+                    Search Finder
+                  </a>
+                </li>
+
+                <li>
+                  <a class="text-gray-900 font-normal text-base transition hover:text-gray-500/75" href="/">
+                    Settings
+                  </a>
+                </li>
+                <li>
+
+                  <div class="relative mr-3">
+                    <input
+                      class="block px-5 py-2.5  pr-16 text-sm border-1.5 border-teal-600 rounded-lg"
+                      id="search-url"
+                      type="url"
+                      placeholder="Insert search URL here"
+                    />
+
+                    <button class="absolute p-2 text-white -translate-y-1/2  bg-teal-600 rounded-full top-1/2 right-4" type="button">
+                      <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="white ">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                      </svg>
+                    </button>
+                  </div>
+
+                </li>
+
+              </ul>
+            </nav>
+
+            <div class="flex items-center gap-4">
+              <div class="sm:gap-4 sm:flex">
+                <a
+                  class="block px-5 py-2.5 text-sm font-medium text-white bg-teal-600 hover:bg-teal-700 transition rounded-md"
+                  href="/"
+                >
+                  Login
+                </a>
+
+                <a
+                  class="hidden sm:block px-5 py-2.5 text-sm font-medium text-teal-600 bg-gray-100 rounded-md hover:text-teal-600/75 transition"
+                  href="/signuppage">
+                  Register
+                </a>
+              </div>
+
+              <button
+                class="block p-2.5 text-gray-600 transition bg-gray-100 rounded md:hidden hover:text-gray-600/75">
+                <span class="sr-only">Toggle menu</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
+      </header>
 
-        <button
-          class="block p-2.5 text-gray-600 transition bg-gray-100 rounded md:hidden hover:text-gray-600/75">
-          <span class="sr-only">Toggle menu</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-5 h-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M4 6h16M4 12h16M4 18h16"/>
-          </svg>
-        </button>
-      </div>
     </div>
-  </div>
-</header>
-            
-        </div>
-    )
+  )
 }
